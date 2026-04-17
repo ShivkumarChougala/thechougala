f129ddf3253857b4a03527d789bc2b5a1466979a
ShivkumarChougala <shivakumarchougala9@gmail.com>
2026-04-18 00:31:53 +0530

    Add: projects in json

---
+++ static/projects.js
@@ -0,0 +1,12 @@
+const PROJECTS = [
+  {
+    title: "GhostTrap",
+    desc: "AI-powered SSH honeypot for analyzing attacker behavior",
+    link: "https://github.com/ShivkumarChougala/ghosttrap"
+  },
+  {
+    title: "SurfaceWatch",
+    desc: "System exposure and attack surface analysis tool",
+    link: "https://github.com/ShivkumarChougala/SurfaceWatch"
+  }
+];
