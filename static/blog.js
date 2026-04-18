dfca881e3448964f3d4ac303b9055d375af46181
ShivkumarChougala <shivakumarchougala9@gmail.com>
2026-04-18 21:25:23 +0530

    Added blog links

---
+++ static/blog.js
@@ -0,0 +1,16 @@
+const BLOGS = [
+	
+ {
+    title: "Cracking CrackIt",
+    desc: "Reverse Engineering Walkthrough",
+    link: "https://medium.com/@an0th3rh4ck/cracking-crackit-crackmes-one-a-step-by-step-reverse-engineering-walkthrough-59db8043e4a4"
+  },
+
+  {
+    title: "Pi-hole on My Raspberry Pi",
+    desc: "Accessed Pi-hole From Anywhere",
+    link: "https://medium.com/@an0th3rh4ck/how-i-blocked-ads-for-my-whole-home-and-accessed-pi-hole-from-anywhere-749ee5a762aa"
+  },
+  
+];
+   
