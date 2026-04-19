8a9de7650b426e0bdc343bf23000bc88432e1076
ShivkumarChougala <shivakumarchougala9@gmail.com>
2026-04-19 21:56:47 +0530

    added medium links of blogs

--- static/blog.js
+++ static/blog.js
@@ -1,16 +1,21 @@
 const BLOGS = [
-	
- {
+
+  {
+    title: "GhostTrap: AI-Powered SSH Honeypot",
+    desc: "Building a real-time SSH honeypot with AI and dashboard",
+    link: "https://medium.com/@thechougala/building-ghosttrap-my-ai-powered-ssh-honeypot-66e1dcc7a6d3"
+  },
+
+  {
     title: "Cracking CrackIt",
     desc: "Reverse Engineering Walkthrough",
-    link: "https://medium.com/@an0th3rh4ck/cracking-crackit-crackmes-one-a-step-by-step-reverse-engineering-walkthrough-59db8043e4a4"
+    link: "https://medium.com/@thechougala/cracking-crackit-crackmes-one-a-step-by-step-reverse-engineering-walkthrough-59db8043e4a4"
   },
 
   {
     title: "Pi-hole on My Raspberry Pi",
     desc: "Accessed Pi-hole From Anywhere",
-    link: "https://medium.com/@an0th3rh4ck/how-i-blocked-ads-for-my-whole-home-and-accessed-pi-hole-from-anywhere-749ee5a762aa"
+    link: "https://medium.com/@thechougala/how-i-blocked-ads-for-my-whole-home-and-accessed-pi-hole-from-anywhere-749ee5a762aa"
   },
-  
+
 ];
-   
