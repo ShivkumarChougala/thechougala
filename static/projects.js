0e3a7812291ccd63b3c9be94234a776e47bae03b
ShivkumarChougala <shivakumarchougala9@gmail.com>
2026-04-21 23:29:51 +0530

    added latest project

--- static/projects.js
+++ static/projects.js
@@ -4,6 +4,12 @@ const PROJECTS = [
     desc: "AI-powered SSH honeypot for analyzing attacker behavior",
     link: "https://github.com/ShivkumarChougala/ghosttrap"
   },
+{
+    title: "ai-security-platform",
+    desc: "A playground where you attack real vulnerabilities and see exactly what happens inside the system. ",
+    link: "https://github.com/ShivkumarChougala/ai-security-platform"
+  }
+
   {
     title: "SurfaceWatch",
     desc: "System exposure and attack surface analysis tool",
