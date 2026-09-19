ae6adbadbd2f4d7986e43aea5512452a0b9e9cf4
ShivkumarChougala <shivakumarchougala9@gmail.com>
2026-09-19 21:00:42 +0530

    Add hostmonitor and openHouse projects

--- static/projects.js
+++ static/projects.js
@@ -1,8 +1,20 @@
 const PROJECTS = [
   {
     title: "DrishtiMesh",
-    desc: "Distributed honeypot mesh for one-click threat collection and real-time IP reputation intelligence.",
+    desc: "Distributed honeypot mesh for one-click threat collection and real-time IP repu intelligence.",
     link: "https://github.com/ShivkumarChougala/drishtimesh"
+  },
+
+ {
+    title: "hostmonitor",
+    desc: "HostMonitor is a lightweight, real-time monitoring dashboard for Linux hosts",
+    link: "https://github.com/ShivkumarChougala/hostmonitor"
+  },
+
+ {
+    title: "openHouse",
+    desc: "Secure your network, block unwanted content, and monitor all connected devices in real time.s",
+    link: "https://github.com/ShivkumarChougala/openHouse"
   },
 
   {
